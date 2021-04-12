6a8bdb540778f2fa5a2b5feba1c55d2eafc44bd4
Einar Rasmussen <einar@di.ku.dk>
2021-04-12 14:34:30 +0200

    Website working

--- src/itzibitzi.js
+++ src/itzibitzi.js
@@ -1,49 +1,71 @@
-#!/usr/bin/env node
+// JavaScript's `typeof` operator leaves some things to be desired, so we roll our own.
+function typeof$ (value) {
+    const v = typeof value
 
-const fs = require('fs')
+    switch (v) {
 
-async function getInputObject () {
-  // Get input
-  let input
-  try {
-    input = fs.readFileSync(process.stdin.fd, 'utf-8')
-  } catch (error) {
-    console.error('No input supplied.  Try: `echo {"a": {"b": {"c": 42}}} |  node fdsf`')
-    process.exit()
+      case 'object':
+
+        if (value === null) {
+            return 'null'
+        }
+
+        if (value instanceof Object) {
+            return 'object'
+        }
+        
+        if (value instanceof Array) {
+            return 'array'
+        }
+        // fallthrough for odd objects
+
+      default:
+        return v
+        break
+    }
+}
+
+// We do not want at dot in front of the empty path.
+function suffix (path, key) {
+  if (path === '') {
+    return key
   }
 
-  console.info(`read: ${input}`)
+  return (path + '.' + key)
+}
 
-  // Parse input
-  let parsed
-  try {
-    parsed = JSON.parse(input)
-  } catch (error) {
-    console.error(`The input: ${input}`)
-    process.exit()
+// The business logic is here.
+function evaluator(path, key, value) {
+  switch (typeof$(value)) {
+
+    case 'object':
+        const entries = Object.entries(value)
+        return entries.flatMap(([k, v]) => evaluator(suffix(path, key), k, v))
+        break
+
+    case 'array':
+        throw Error('Arrays are not supported')
+        break
+
+    default:
+        // value is a primitive type
+        return [[suffix(path, key), value]]
   }
-
-  return parsed
 }
 
-async function flatten (obj) {
-  return obj
+function flatten (obj) {
+  const initPath = ''
+  const initKey  = ''
+  const rvArr    = evaluator(initPath, initKey, obj)
+  return Object.fromEntries(rvArr)
 }
 
-async function writeOutputObject (flatObj) {
-  console.info(`result: ${flatObj}`)
-  return flatObj
+function parseAndFlatten (rawtxt) {
+  /* JSON.parse() is a picky eater.  We should use a more comprehensive deprettyfier,
+  but for demonstration purposes, this will do */
+  const preprocess = rawtxt.replace(/\n/g, '').replace(/\s{2,}/g, ' ')
+  const parsed     = JSON.parse(prep)
+  const flatObj    = flatten(parsed)
+  const prettyStr  = JSON.stringify(flatObj, null, 4)
+  return prettyStr
 }
-
-async function main () {
-  console.info('start')
-
-  const parsed = await getInputObject()
-  const output = await flatten(parsed)
-  writeOutputObject(output)
-
-  console.info('done')
-  process.exit()
-}
-
-main()
