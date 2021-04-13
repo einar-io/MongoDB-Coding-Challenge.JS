05d8c6738883024a15c08a571dca4e8a907ad721
Einar Rasmussen <einar@di.ku.dk>
2021-04-13 20:35:08 +0200

    Factored out the functions for the CLI.

---
+++ src/cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const getStdin = require('get-stdin')
+const itzibitzi = require('./itzibitzi.js')
+
+const EXIT_SUCCESS = 0
+const EXIT_FAILURE = 1
+
+async function main () {
+  const usrtxt = await getStdin()
+
+  if (usrtxt === "") {
+    console.error('No input supplied.  Try: `echo \'{ "a": 1, "b": true, "c": { "d": 3, "e": "test" }}\' |  node src/cli.js`')
+    process.exit(EXIT_FAILURE)
+  }
+
+  const prettyStr = itzibitzi.parseAndFlatten(usrtxt)
+  console.log(prettyStr)
+
+  return prettyStr
+  process.exit(EXIT_SUCCESS)
+}
+
+main()
